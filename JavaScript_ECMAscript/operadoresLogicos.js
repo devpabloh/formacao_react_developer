6565e9567e3482b554830d62edde856b6ac3e8b2
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-07 10:41:32 -0300

    Create operadoresLogicos.js

---
+++ JavaScript_ECMAscript/operadoresLogicos.js
@@ -0,0 +1,11 @@
+/* 
+Operadores lógicos
+&& significa E
+|| significa OU
+== significa valor igual
+=== Significa valor e tipo igual
+!= significa diferente valor
+!== significa diferente valor e tipo
+> significa maior que
+< significa menor que
+*/
