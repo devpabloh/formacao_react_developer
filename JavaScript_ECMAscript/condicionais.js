ccb4879b992f3f60748ec5a446becb2abed2df97
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-07 20:41:00 -0300

    Update condicionais.js

--- JavaScript_ECMAscript/condicionais.js
+++ JavaScript_ECMAscript/condicionais.js
@@ -1,10 +1,13 @@
 
-idade = 16
+let nota = 7
 
-if(idade > 18){
-    console.log(`você é maior de idade`)
-}else{
-    console.log(`Você é menor de idade`)
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
 }
 
 
