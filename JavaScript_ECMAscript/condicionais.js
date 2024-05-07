8a27dc575d6a05b91a3ae83917d9bf26c287d4f0
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-07 20:24:24 -0300

    atualização

---
+++ JavaScript_ECMAscript/condicionais.js
@@ -0,0 +1,17 @@
+
+
+if(idade > 18){
+    console.log(`você é maior de idade`)
+}else{
+    console.log(`Você é menor de idade`)
+}
+
+idade = 16
+/* switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+} */
