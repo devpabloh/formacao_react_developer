519b0533dff252523b5c36f228fe1d66c9093957
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-08 12:33:08 -0300

    atualização

--- JavaScript_ECMAscript/funcoes.js
+++ JavaScript_ECMAscript/funcoes.js
@@ -28,3 +28,53 @@ function somar(num3, num4){
 
 console.log(somar(1,2)) 
 
+let alunos = [
+    {
+        nome:"Roberto",
+        idade:16,
+    },
+    {
+        nome: "Pablo",
+        idade: 18,
+
+    },
+    {
+        nome: 'Tais',
+        idade: 15,
+
+    },
+    {
+        nome: "Neide",
+        idade: 25,
+
+    },
+    {
+        nome: 'Teo',
+        idade: 5,
+    },
+]
+
+/* 
+
+Funcionalidades do ES6+
+
+-filter () filtra determinado chave ou valor que foi solicitado 
+
+-find () retorna o primeiro item que foi encontrado dentro do array/objeto 
+
+-findindex() retorna o index, ou seja, o numero do indexador aonde se encontra o valor/parametro que foi solicitado
+
+- reduce() Quando queremos pegar algum tipo de informação e fazer um calcúlo
+sintaxe
+const idades = alunos.reduce((acc, aluno)=>{
+    return acc + aluno.idade
+},0)
+
+- some() retorna true, se apenas uma condição for verdadeira
+
+let acimaDeNove = alunos.some(alunos => alunos.idade < 10);
+
+- every() retorna true, se todas as condições forem verdadeira
+
+let acimaDeNove = alunos.every(alunos => alunos.idade < 60);
+*/
