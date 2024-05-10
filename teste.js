ce7961e30aa93acef2451baf317d86015345de6c
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-10 11:58:36 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,13 @@
+
+
+var array = [ 2, 3, 5, 7, 11, 13, 18, 34 ];
+
+//TODO: Complete o laço de repetição:
+for (let i = 0; i <= array.length; i++){
+//TODO: Agora crie uma condição que verifique o ARRAY e imprima APENAS os números pares.
+  if(array[i] % 2 === 0){
+    console.log(array[i]);
+  }
+}
+
+
